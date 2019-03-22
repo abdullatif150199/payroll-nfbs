761f8b5ccd449184e98e83216ac1184cf1e34edb
Akmam MD <akhmami@gmail.com>
2019-03-22 14:36:43 +0700

    update factory karyawan

--- database/factories/KaryawanFactory.php
+++ database/factories/KaryawanFactory.php
@@ -20,12 +20,12 @@ $factory->define(App\Karyawan::class, function (Faker $faker) {
             return App\Unit::inRandomOrder()->first()->id;
         },
         'no_induk' => mt_rand(100000, 300000),
+        'nik' => mt_rand(1000000000000000, 3000000000000000),
         'nama_lengkap' => $faker->name,
         'tempat_lahir' => $faker->city,
         'tanggal_lahir' => $faker->date(),
         'jenis_kelamin' => $faker->randomElement(['L', 'P']),
         'status_pernikahan' => $faker->randomElement(['M', 'B']),
-        'jumlah_anak' => mt_rand(0, 12),
         'alamat' => $faker->address,
         'no_hp' => $faker->phoneNumber,
         'nama_pendidikan' => $faker->company,
