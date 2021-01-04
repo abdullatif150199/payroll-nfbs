28bce9226737ac586c86a79d482b1309639536a6
Akmam MD <akhmami@gmail.com>
2021-01-04 15:07:58 +0700

    force to https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (config('app.env') === 'production') {
+        if (config('app.env') === 'testing') {
             \URL::forceScheme('https');
         }
     }
