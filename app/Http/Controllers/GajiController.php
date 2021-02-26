a3f3e3210a9352ec47766de0a1dda794e8278dcc
Akmam MD <akhmami@gmail.com>
2021-02-26 14:57:17 +0700

    pajax

--- app/Http/Controllers/GajiController.php
+++ app/Http/Controllers/GajiController.php
@@ -131,7 +131,7 @@ class GajiController extends Controller
 
             // update atau create tax history
             $gaji->taxHistory()->updateOrCreate([
-                'id' => $gaji->taxHistory->id
+                'id' => $gaji->taxHistory->id ?? null
             ], [
                 'gaji_perbulan' => $gatot,
                 'gaji_pertahun' => $gaji_pertahun,
