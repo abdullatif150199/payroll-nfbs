16b0db384d7ee9d2a6f6659489089587e9f8c783
MD Akmami <akhmami@gmail.com>
2022-03-16 10:41:34 +0700

    add gaji first user

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -13,7 +13,7 @@
     <div class="row">
         <div class="col">
             <div class="p-2">
-                <div class="text-muted"><small>Gaji Bulan {{ yearMonth($item->bulan, 'H') }}</small></div>
+                <div class="text-muted"><small>Gaji Bulan {{ yearMonth($gajiFirst->bulan, 'H') }}</small></div>
                 <h3 class="font-weight-bold">Rp {{ $gajiFirst->gaji_akhir }}</h3>
             </div>
         </div>
