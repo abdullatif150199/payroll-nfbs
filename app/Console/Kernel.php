bad181af20ace3e21452a9a7b424a0680ac26e2e
MD Akmami <akhmami@gmail.com>
2022-04-05 09:52:48 +0700

    schedule every 30 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('scanlog:nonshift')
                 ->withoutOverlapping()
-                ->everyMinute();
+                ->everyThirtyMinutes();
         $schedule->command('scanlog:shift')
                 ->withoutOverlapping()
                 ->everyThirtyMinutes();
