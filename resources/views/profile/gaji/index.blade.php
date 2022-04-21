bdf0384cfb1c5a1b91a8757f648d4fa23a6f7a84
MD Akmami <akhmami@gmail.com>
2022-04-21 17:26:34 +0700

    fixed paginate in profile

--- resources/views/profile/gaji/index.blade.php
+++ resources/views/profile/gaji/index.blade.php
@@ -1,32 +1,4 @@
 @extends('layouts.profile')
-@push('styles')
-<link rel="stylesheet" href="{{ asset('css/dataTables.bootstrap4.min.css') }}">
-<style>
-    .dataTables_length {
-        padding-left: 1rem;
-        padding-top: 1rem;
-    }
-
-    .dataTables_filter {
-        padding-right: 1rem;
-        padding-top: 1rem;
-    }
-
-    .dataTables_info {
-        padding-left: 1rem;
-        padding-bottom: 1rem;
-    }
-
-    .dataTables_paginate {
-        padding-right: 1rem;
-        padding-bottom: 1rem;
-    }
-
-    .paginate_button .page-link {
-        padding: 0.3rem 0.5rem
-    }
-</style>
-@endpush
 @section('content')
 <div class="col-lg-8">
     <ul class="list-group">
