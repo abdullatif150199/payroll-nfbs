31398a1a9d2888469ba172375d18ab63c6c90f48
MD Akmami <akhmami@gmail.com>
2022-06-20 10:17:16 +0700

    fixed import

--- app/Exports/FormatKinerja.php
+++ app/Exports/FormatKinerja.php
@@ -4,6 +4,7 @@ namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use App\Models\PersentaseKinerja;
+use App\Models\Karyawan;
 
 class FormatKinerja implements WithHeadings
 {
@@ -15,10 +16,26 @@ class FormatKinerja implements WithHeadings
             'Unit',
         ];
         $kinerja = PersentaseKinerja::pluck('title')->toArray();
-
-        return [
+        $data = [
             array_merge($head, $kinerja),
             ['123456', 'Fulan', 'Personalia', '100', '80']
         ];
+        if (auth()->user()->hasRole(['root', 'admin'])) {
+            $data = [
+                array_merge($head, $kinerja)
+            ];
+            $karyawan = Karyawan::with(['unit'])->cursor();
+            foreach ($karyawan as $kar) {
+                $data[] = [
+                    $kar->no_induk,
+                    $kar->nama_lengkap,
+                    implode(',', $kar->unit->pluck('nama_unit')->toArray()),
+                    '',
+                    ''
+                ];
+            }
+        }
+        
+        return $data;
     }
 }
