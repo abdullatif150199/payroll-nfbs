2b91b41df4991a2d241a1ac7462c9b80fe787ca6
reseller newby <resellernewby@gmail.com>
2023-03-02 07:58:14 +0700

    urutkan tampilah kehadiran pada profile user berdasarkan tanggal

--- app/Http/Controllers/Profile/KehadiranController.php
+++ app/Http/Controllers/Profile/KehadiranController.php
@@ -10,7 +10,7 @@ class KehadiranController extends ProfileController
     {
         $data = Kehadiran::with('karyawan')
             ->where('karyawan_id', $this->getId())
-            ->latest()
+            ->latest('tanggal')
             ->simplePaginate(10);
 
         return view('profile.kehadiran.index', [
